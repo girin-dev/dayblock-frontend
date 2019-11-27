6799a6aa7ea1faba285fcaa83f837058cbc2128b
girin-dev <kw8799@gmail.com>
2019-11-28 00:09:00 +0900

    [KanbanSchedule] Datetime 컴포넌트 분리, 칸반선택 State로 관리

--- src/componenets/kanbanSchedule/KanbanSchedule.js
+++ src/componenets/kanbanSchedule/KanbanSchedule.js
@@ -1,42 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './KanbanSchedule.css';
 import Schedule from './Schedule';
 import Kanban from '../kanban/Kanban';
+import Datetime from './Datetime';
 
-const dateSelector=_=>{
-  const today = new Date();
-  const year  = today.getFullYear(); 
-  const month  = today.getMonth() + 1; 
-  const day  = `${today.getDate()}`.padStart(2, '0'); 
-  return (
-    // <div key={index} style={style} className="schedule-date">
-    <div className="date">
-      <button className="btn btn-date">&#60;</button>
-      <span className="date-selected">{`${year}-${month}-${day}`}</span>
-      <button className="btn btn-date">&#62;</button>
-    </div>
-    // </div>
-  )
-}
+const initData = [
+  {id:1, title:"Suzy's diary"},
+  {id:2, title:"Suzy's diary1"},
+  {id:3, title:"Suzy's diary2"},
+  {id:4, title:"Suzy's diary3"},
+  {id:5, title:"Suzy's diary4"},
+];
 const KanbanSchedule = () => {
+    const [kanbanList, setKanbanList] = useState(initData);
+    const [kanban, setKanban] = useState(kanbanList[0].id);
+    const onChange = e =>{
+      setKanban(parseInt(e.target.value))
+    }
     return (
       <main className="main">
         <div className="time-selector">
-          {dateSelector()}
+          <Datetime/>
         </div>
         <div className="block-selector">
           <div className="schedule-info">S</div>
-          <select className="schedule-select">
-            <option value="Suzy's diary">Suzy's diary</option>
-            <option value="Suzy's diary2">Suzy's diary1</option>
-            <option value="Suzy's diary3">Suzy's diary2</option>
-            <option value="Suzy's diary4">Suzy's diary3</option>
+          <select className="schedule-select" onChange={onChange}>
+            { kanbanList.map(kanban=><option key={kanban.id} value={kanban.id}>{kanban.title}</option>)
+            }
           </select>
         </div>
         <Schedule></Schedule>
-        <Kanban></Kanban>
+        <Kanban kanban={kanban}></Kanban>
       </main>
     );
 };
-
 export default KanbanSchedule;
